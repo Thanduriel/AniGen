381923db626e443b144a892f146fc108d687507b
Thanduriel <rojendersie@alice.de>
2021-06-21 00:40:03 +0200

    + multithearding support + distance in transfer map visualization

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <SFML/Graphics.hpp>
 #include <random>
 #include <functional>
+#include <thread>
+#include <chrono>
+#include <iostream>
 
 // dynamic sized matrix
 template<typename T>
@@ -11,14 +14,21 @@ struct Matrix
 	Matrix() = default;
 	Matrix(const sf::Vector2u & _size) : size(_size)
 	{
-		elements.resize(size.x * size.y);
+		elements.resize(static_cast<size_t>(size.x) * static_cast<size_t>(size.y));
 	}
 
+	const T& operator()(unsigned x, unsigned y) const { return elements[flatIndex(x, y)]; }
 	T& operator()(unsigned x, unsigned y) { return elements[flatIndex(x,y)]; }
-	size_t flatIndex(unsigned x, unsigned y) const { return x + y * size.x; }
+
+	size_t flatIndex(unsigned x, unsigned y) const 
+	{ 
+		return x + static_cast<size_t>(y) * size.x; 
+	}
+
 	sf::Vector2u index(size_t flat) const 
 	{ 
-		return sf::Vector2u(static_cast<unsigned>(flat % size.x), static_cast<unsigned>(flat / size.y)); 
+		return sf::Vector2u(static_cast<unsigned>(flat % size.x), 
+			static_cast<unsigned>(flat / size.y)); 
 	}
 
 	sf::Vector2u size;
@@ -76,14 +86,16 @@ auto applyConvolution(const sf::Image& _image, const Matrix<T>& _kernel, Distanc
 	{
 		ReturnType sum{};
 		for (unsigned i = 0; i < _kernel.size.y; ++i)
+		{
+			const int yInd = static_cast<int>(y + i) - kernelHalf.y;
 			for (unsigned j = 0; j < _kernel.size.x; ++j)
 			{
 				// use padding if index is negative
 				const int xInd = static_cast<int>(x + j) - kernelHalf.x;
-				const int yInd = static_cast<int>(y + i) - kernelHalf.y;
 				const auto d = _dist(_kernel.elements[j + i * _kernel.size.x], getPixelPadded(_image, xInd, yInd));
 				sum = _reduce(sum, d);
 			}
+		}
 		return sum;
 	};
 
@@ -100,12 +112,12 @@ auto applyConvolution(const sf::Image& _image, const Matrix<T>& _kernel, Distanc
 }
 
 // constructs a map that transforms _src to _dst.
-TransferMap constructMap(const sf::Image& _src, const sf::Image& _dst)
+TransferMap constructMap(const sf::Image& _src, const sf::Image& _dst, unsigned _numThreads = 1)
 {
 	const sf::Vector2u size = _src.getSize();
 
 	const sf::Vector2u kernelSize(3, 3);
-	sf::Vector2i kernelHalf(kernelSize.x / 2, kernelSize.y / 2);
+	const sf::Vector2i kernelHalf(kernelSize.x / 2, kernelSize.y / 2);
 	auto makeKernel = [&](unsigned x, unsigned y)
 	{
 		Matrix<sf::Color> kernel(kernelSize);
@@ -132,25 +144,44 @@ TransferMap constructMap(const sf::Image& _src, const sf::Image& _dst)
 	};
 
 	TransferMap map(size);
-	for (unsigned y = 0; y < size.y; ++y)
-	{
-		for (unsigned x = 0; x < size.x; ++x)
-		{
-			Matrix<int> similarity = applyConvolution(_dst, makeKernel(x,y), distance, sum);
 
-			// default guess is identity
-			sf::Vector2u maxPos(x,y);
-			int max = similarity(x,y);
-			for (size_t i = 0; i < similarity.elements.size(); ++i)
+	auto computeRows = [&](unsigned begin, unsigned end)
+	{
+		for (unsigned y = begin; y < end; ++y)
+		{
+			for (unsigned x = 0; x < size.x; ++x)
 			{
-				if (max < similarity.elements[i])
+				Matrix<int> similarity = applyConvolution(_dst, makeKernel(x, y), distance, sum);
+
+				// default guess is identity
+				sf::Vector2u maxPos(x, y);
+				int max = similarity(x, y);
+				for (size_t i = 0; i < similarity.elements.size(); ++i)
 				{
-					max = similarity.elements[i];
-					maxPos = similarity.index(i);
+					if (max < similarity.elements[i])
+					{
+						max = similarity.elements[i];
+						maxPos = similarity.index(i);
+					}
 				}
+				map(x, y) = maxPos;
 			}
-			map(x, y) = maxPos;
 		}
+	};
+	if(_numThreads == 1)
+		computeRows(0, size.y);
+	else 
+	{
+		std::vector<std::thread> threads;
+		threads.reserve(_numThreads - 1);
+
+		const unsigned rows = size.y / _numThreads;
+		for (unsigned i = 0; i < _numThreads - 1; ++i)
+			threads.emplace_back(computeRows, i * rows, (i + 1) * rows);
+		computeRows((_numThreads - 1) * rows, size.y);
+
+		for (auto& thread : threads)
+			thread.join();
 	}
 
 	return map;
@@ -180,11 +211,32 @@ sf::Image imageDifference(const sf::Image& a, const sf::Image& b)
 	return difImage;
 }
 
+sf::Image distanceMap(const TransferMap& transferMap)
+{
+	sf::Image distImage;
+	const sf::Vector2u size = transferMap.size;
+	distImage.create(size.x, size.y);
+
+	for (unsigned y = 0; y < size.y; ++y)
+	{
+		for (unsigned x = 0; x < size.x; ++x)
+		{
+			sf::Color dist;
+			const sf::Vector2u& v = transferMap(x, y) - sf::Vector2u(x,y);
+			dist.g = static_cast<sf::Uint8>(std::abs(static_cast<float>(v.x) / size.x) * 255.f);
+			dist.b = static_cast<sf::Uint8>(std::abs(static_cast<float>(v.y) / size.y) * 255.f);
+			dist.a = 255;
+			distImage.setPixel(x, y, dist);
+		}
+	}
+	return distImage;
+}
+
 int main()
 {
+	auto start = std::chrono::high_resolution_clock::now();
 	SpriteSheet reference("../Sprites/HumanMale_ApronChest.png", 8);
 	SpriteSheet validation("../Sprites/HumanMale_FamilyChest_Medium.png", 8);
-	validation.save("yolo.png");
 
 	SpriteSheet output;
 	SpriteSheet dif;
@@ -192,7 +244,8 @@ int main()
 	dif.frames.emplace_back(imageDifference(validation.frames[0], output.frames[0]));
 	for (int i = 1; i < 8; ++i)
 	{
-		const TransferMap map = constructMap(reference.frames[0], reference.frames[i]);
+		const TransferMap map = constructMap(reference.frames[0], reference.frames[i], 4);
+		distanceMap(map).saveToFile("distance" + std::to_string(i) + ".png");
 		output.frames.emplace_back(applyMap(map, validation.frames[i]));
 		dif.frames.emplace_back(imageDifference(validation.frames[i], output.frames.back()));
 	}
@@ -207,6 +260,9 @@ int main()
 	}*/
 	dif.save("dif.png");
 	output.save("output.png");
+
+	auto end = std::chrono::high_resolution_clock::now();
+	std::cout << std::chrono::duration<double>(end - start).count() << std::endl;
 
 	return 0;
 }
